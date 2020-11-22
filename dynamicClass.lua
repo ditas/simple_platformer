135ab3e2e35c7884c7a8d73b1c6759a31a11a8f3
dmitry ditas <ditasandditas@gmail.com>
2020-11-22 00:36:36 +0000

    Handle detailed action change on collision

--- dynamicClass.lua
+++ dynamicClass.lua
@@ -27,6 +27,21 @@ end
 
 function Dynamic:update(dt, obstacles)
     print("ACTION " .. self.action)
+
+    if self.action == "Right side crossed with obstacle's Bottom" 
+        or self.action == "Left side crossed with obstacle's Bottom" 
+    then
+        self.action = "freeFall"
+        self.baseSpeed = 0
+    elseif self.action == "Top side crossed with obstacle's Left"
+        or self.action == "Bottom side crossed with obstacle's Left"
+        or self.action == "Top side crossed with obstacle's Right"
+        or self.action == "Bottom side crossed with obstacle's Right"
+    then
+        self.action = "freeFall"
+        self.baseSpeed = self.maxSpeed
+    end
+
     self.x = self.x
     if self.action == "freeFall" then
         self:freeFallDelta(dt) -- без разницы вызывать собственный метод через self./self:
@@ -42,11 +57,11 @@ function Dynamic:update(dt, obstacles)
     elseif self.action == "throwAngle" then
         Dynamic.throwAngleDelta(self, dt)
     end
-    -- detectCollision(obstacles)
+    Dynamic.detectCollision(self, obstacles)
 end
 
 function Dynamic:freeFallDelta(t)
-    print("down V " .. self.baseSpeed)
+    -- print("down V " .. self.baseSpeed)
     if self.baseSpeed < self.maxSpeed then
         speed = self.baseSpeed + g*t
         self.y = self.y + speed
@@ -67,8 +82,10 @@ function Dynamic:throwUpDelta(t)
 end
 
 function Dynamic:throwUp(v)
-    self.baseSpeed = v
-    self.action = "throwUp"
+    if self.action ~= "Right side crossed with obstacle's Bottom" and self.action ~= "Left side crossed with obstacle's Bottom" then
+        self.baseSpeed = v
+        self.action = "throwUp"
+    end
 end
 
 function Dynamic:throwAngleDelta(t)
@@ -81,19 +98,122 @@ function Dynamic:throwAngleDelta(t)
 end
 
 function Dynamic:throwAngle(v, alpha, throwAngleTimeMultiplier)
-    self.fixX = self.x
-    self.fixY = self.y
-    -- print("FIX X: " .. self.fixX .. " FIX Y: " .. self.fixY)
-    self.angle = alpha*math.pi/180
-    self.baseSpeed = v
-    self.action = "throwAngle"
-    self.time = 0
-    self.throwAngleTimeMultiplier = throwAngleTimeMultiplier or 10
+    if self.action ~= "Right side crossed with obstacle's Bottom" 
+        and self.action ~= "Left side crossed with obstacle's Bottom" 
+        and self.action ~= "Top side crossed with obstacle's Left"
+        and self.action ~= "Bottom side crossed with obstacle's Left"
+        and self.action ~= "Top side crossed with obstacle's Right"
+        and self.action ~= "Bottom side crossed with obstacle's Right"
+    then
+        self.fixX = self.x
+        self.fixY = self.y
+        -- print("FIX X: " .. self.fixX .. " FIX Y: " .. self.fixY)
+        self.angle = alpha*math.pi/180
+        self.baseSpeed = v
+        self.action = "throwAngle"
+        self.time = 0
+        self.throwAngleTimeMultiplier = throwAngleTimeMultiplier or 10
+    end
+end
+
+function Dynamic:detectCollision(obstacles)
+
+    -- local left = {x1 = self.x, y1 = self.y, x2 = self.x, y2 = self.y + self.height}
+    -- local right = {x1 = self.x + self.width, y1 = self.y, x2 = self.x + self.width, y2 = self.y + self.height}
+
+    -- local top = {x1 = self.x, y1 = self.y, x2 = self.x + self.width, y2 = self.y}
+    -- local bottom = {x1 = self.x, y1 = self.y + self.height, x2 = self.x + self.width, y2 = self.y + self.height}
+
+    local left = {x1 = self.x, y1 = self.y - 5, x2 = self.x, y2 = self.y + self.height + 5}
+    local right = {x1 = self.x + self.width, y1 = self.y - 5, x2 = self.x + self.width, y2 = self.y + self.height + 5}
+
+    local top = {x1 = self.x - 5, y1 = self.y, x2 = self.x + self.width + 5, y2 = self.y}
+    local bottom = {x1 = self.x - 5, y1 = self.y + self.height, x2 = self.x + self.width + 5, y2 = self.y + self.height}
+
+    for i,o in ipairs(obstacles) do
+
+        -- print("------------OBSTACLE " .. o.x)
+
+        local o_left = {x1 = o.x, y1 = o.y, x2 = o.x, y2 = o.y + o.height}
+        local o_right = {x1 = o.x + o.width, y1 = o.y, x2 = o.x + o.width, y2 = o.y + o.height}
+
+        local o_top = {x1 = o.x, y1 = o.y, x2 = o.x + o.width, y2 = o.y}
+        local o_bottom = {x1 = o.x, y1 = o.y + o.height, x2 = o.x + o.width, y2 = o.y + o.height}
+
+        inter1 = checkIntersection(left, o_top)
+        inter2 = checkIntersection(right, o_top)
+
+        inter3 = checkIntersection(left, o_bottom)
+        inter4 = checkIntersection(right, o_bottom)
+
+        -- if inter1 or inter2 then
+        --     self.action = "side_top"
+        -- elseif inter3 or inter4 then
+        --     self.action = "side_bottom"
+        --     -- self.baseSpeed = 0
+        -- end
+
+        inter5 = checkIntersection(top, o_left)
+        inter6 = checkIntersection(bottom, o_left)
+
+        inter7 = checkIntersection(top, o_right)
+        inter8 = checkIntersection(bottom, o_right)
+
+        if inter1 then
+            self.action = "Left side crossed with obstacle's Top"
+        end
+        if inter2 then
+            self.action = "Right side crossed with obstacle's Top"
+        end
+
+        if inter3 then
+            self.action = "Left side crossed with obstacle's Bottom"
+        end
+        if inter4 then
+            self.action = "Right side crossed with obstacle's Bottom"
+        end
+
+
+
+        if inter5 then
+            self.action = "Top side crossed with obstacle's Left"
+        end
+        if inter6 then
+            self.action = "Bottom side crossed with obstacle's Left"
+        end
+        if inter7 then
+            self.action = "Top side crossed with obstacle's Right"
+        end
+        if inter8 then
+            self.action = "Bottom side crossed with obstacle's Right"
+        end
+
+        -- inter5 = checkIntersection(top, o_left)
+        -- inter6 = checkIntersection(bottom, o_left)
+
+        -- inter7 = checkIntersection(top, o_right)
+        -- inter8 = checkIntersection(bottom, o_right)
+
+        -- if inter5 or inter6 or inter7 or inter8 then
+        --     self.action = "side_side"
+        --     -- self.baseSpeed = 0
+        -- end
+    end
+end
+
+function checkIntersection(a, b)
+    v1 = (b.x2-b.x1)*(a.y1-b.y1)-(b.y2-b.y1)*(a.x1-b.x1)
+    v2 = (b.x2-b.x1)*(a.y2-b.y1)-(b.y2-b.y1)*(a.x2-b.x1)
+    v3 = (a.x2-a.x1)*(b.y1-a.y1)-(a.y2-a.y1)*(b.x1-a.x1)
+    v4 = (a.x2-a.x1)*(b.y2-a.y1)-(a.y2-a.y1)*(b.x2-a.x1)
+
+    print("-----V1 * V2: " .. v1*v2)
+    print("-----V3 * V4: " .. v3*v4)
+
+    return (v1*v2<0) and (v3*v4<0)
 end
 
 function Dynamic:draw()
-    print('DRAW')
+    -- print('DRAW')
     love.graphics.rectangle("line", self.x, self.y, self.width, self.height)
 end
-
--- return Dynamic
--- main.lua
+++ main.lua
@@ -2,20 +2,17 @@ function love.load()
     require("staticClass")
     obstacles = {}
 
-    -- d = require("dynamicClass")
-    -- dynamic = Dynamic:new(d, 500, 0, nil, nil, nil, nil, 10)
-    -- dynamic2 = Dynamic:new(d, 100, 0, nil, nil, nil, nil, 10)
     require("dynamicClass")
     dynamic = Dynamic.new(500, 0, nil, nil, nil, nil, 10)
-    dynamic2 = Dynamic.new(100, 0, nil, nil, nil, nil, 10)
+    -- dynamic2 = Dynamic.new(100, 0, nil, nil, nil, nil, 10)
 end
 
 function love.update(dt)    
     dynamic:update(dt, obstacles)
-    print(dynamic.baseSpeed)
+    -- print(dynamic.baseSpeed)
 
-    dynamic2:update(dt, obstacles)
-    print(dynamic2.baseSpeed)
+    -- dynamic2:update(dt, obstacles)
+    -- print(dynamic2.baseSpeed)
 
     for i,o in ipairs(obstacles) do
         o:update()
@@ -24,7 +21,7 @@ end
 
 function love.draw()
     dynamic:draw()
-    dynamic2:draw()
+    -- dynamic2:draw()
 
     for i,o in ipairs(obstacles) do
         o:draw()
@@ -34,15 +31,15 @@ end
 function love.keypressed(key)
     if key == "up" then
         dynamic:throwUp(3)
-        dynamic2:throwUp(3)
+        -- dynamic2:throwUp(5)
     end
     if key == "left" then
         dynamic:throwAngle(50, 135)
-        dynamic2:throwAngle(50, 135)
+        -- dynamic2:throwAngle(50, 135)
     end
     if key == "right" then
         dynamic:throwAngle(50, 45)
-        dynamic2:throwAngle(50, 45)
+        -- dynamic2:throwAngle(50, 45)
     end
 
     if key == "q" then
